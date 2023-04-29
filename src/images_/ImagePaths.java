4b324ddf0cd321fb63dad54cbb01c57e10fd88eb
danielwolski <80836050+danielwolski@users.noreply.github.com>
2023-04-29 22:43:27 +0200

    v1.41

---
+++ src/images_/ImagePaths.java
@@ -0,0 +1,5 @@
+package images_;
+
+public class ImagePaths {
+    public static final String background = "C:\\Users\\logix\\Desktop\\Nowy folder\\Solar-System-WIP-\\src\\images_\\background.jpg";
+}
