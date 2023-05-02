7d922722e504633c8e7c81895a9bb75e2b90a449
danielwolski <80836050+danielwolski@users.noreply.github.com>
2023-05-02 18:53:39 +0200

    Update v1.43

--- src/images_/ImagePaths.java
+++ src/images_/ImagePaths.java
@@ -5,4 +5,10 @@ public class ImagePaths {
     public static final String background = pathBeg + "\\background.jpg";
     public static final String sunImage = pathBeg + "\\sun.png";
     public static final String jupiterImage = pathBeg + "\\jupiter.png";
+    public static final String saturnImage = pathBeg + "\\saturn.png";
+    public static final String uranusImage = pathBeg + "\\uranus.png";
+    public static final String marsImage = pathBeg + "\\mars.png";
+    public static final String earthImage = pathBeg + "\\earth.png";
+    public static final String moonImage = pathBeg + "\\moon.png";
+
 }
